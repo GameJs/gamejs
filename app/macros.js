b80d6ed3eead89cdf618240a1db0d6552f0407dd
Simon Oberhammer <simon@nekapuzer.at>
2010-11-23 18:56:09 +0100

    relative script and resource urls

--- app/macros.js
+++ app/macros.js
@@ -2,11 +2,11 @@ var config = require('./config');
 exports.resourceHref_macro = function(tag) {
    var appId = tag.parameters[0];
    var path = tag.parameters[1] || '';
-   return ('/' + appId + '/' + path).replace(/\/+/g, '/');
+   return ('../' + appId + '/' + path).replace(/\/+/g, '/');
 };
 
 exports.mainScriptHref_macro = function(tag) {
    var appId = tag.parameters[0];
-   return ['/lib/gamejs/apps', appId, 'main.js'].join('/').replace(/\/+/g, '/');
+   return ['../lib/gamejs/apps', appId, 'main.js'].join('/').replace(/\/+/g, '/');
 };
 
