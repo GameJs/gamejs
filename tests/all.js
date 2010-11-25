25f51a5c09aec6bd05cdfb9b41336c32b607af35
Simon Oberhammer <simon@nekapuzer.at>
2010-11-25 19:42:33 +0100

    add binaryheap unit tests

--- tests/all.js
+++ tests/all.js
@@ -3,6 +3,7 @@ exports.testGamejs = require('./gamejs');
 exports.testSprite = require('./gamejs/sprite');
 exports.testVector = require('./gamejs/utils/vectors');
 exports.testAstar = require('./gamejs/pathfinding');
+exports.testBinaryHeap = require('./gamejs/utils/binaryheap');
 // start the test runner if we're called directly from command line
 if (require.main == module.id) {
     require('test').run(exports);
