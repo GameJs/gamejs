88f3031454d9e9f6ed4faf6a9e19a363a68282f0
Simon Oberhammer <simon@nekapuzer.at>
2010-11-25 19:24:25 +0100

    pathfinding namespace cleanup & more docs

--- tests/gamejs/pathfinding.js
+++ tests/gamejs/pathfinding.js
@@ -1,6 +1,6 @@
 var assert = require('assert');
-var astar = require('gamejs/astar');
-var {Map, point} = require('gamejs/astar/map');
+var astar = require('gamejs/pathfinding/astar');
+var {Map, point} = require('gamejs/pathfinding/astar');
 
 
 var ROUTE_A ={point:{x:19, y:19}, from:{point:{x:19, y:18}, from:{point:{x:19, y:17}, from:{point:{x:18, y:16}, from:{point:{x:18, y:15}, from:{point:{x:17, y:14}, from:{point:{x:16, y:13}, from:{point:{x:15, y:12}, from:{point:{x:14, y:11}, from:{point:{x:13, y:10}, from:{point:{x:12, y:9}, from:{point:{x:11, y:8}, from:{point:{x:10, y:7}, from:{point:{x:9, y:7}, from:{point:{x:8, y:6}, from:{point:{x:7, y:5}, from:{point:{x:6, y:4}, from:{point:{x:5, y:3}, from:{point:{x:4, y:2}, from:{point:{x:3, y:1}, from:{point:{x:2, y:0}, from:{point:{x:1, y:0}, from:{point:{x:0, y:0}, from:null, length:0, score:undefined}, length:100, score:2738}, length:222, score:2819}, length:555, score:3011}, length:752, score:3067}, length:1097, score:3271}, length:1245, score:3278}, length:1424, score:3316}, length:1569, score:3320}, length:1735, score:3345}, length:1877, score:3446}, length:2036, score:3464}, length:2210, score:3497}, length:2352, score:3498}, length:2546, score:3551}, length:2725, score:3589}, length:2938, score:3661}, length:3101, score:3683}, length:3248, score:3689}, length:3361, score:3702}, length:3510, score:3710}, length:3646, score:3746}, length:3783, score:3783};
@@ -20,7 +20,7 @@ exports.testSearch = function () {
    var map = new Map();
    assert.deepEqual(toSimplePoints(astar.findRoute(map, point(0, 0), point(19, 19))), ROUTE_A);
    assert.deepEqual(toSimplePoints(astar.findRoute(map, point(0, 0), point(11, 17))), ROUTE_B);
-  //    print (toSimplePoints(astar.findRoute(point(11, 17), point(19, 19))).toSource());
+  // print (toSimplePoints(astar.findRoute(point(11, 17), point(19, 19))).toSource());
   // assert.deepEqual(toSimplePoints(astar.findRoute(point(11, 17), point(19, 19))), ROUTE_C);
 
 };
